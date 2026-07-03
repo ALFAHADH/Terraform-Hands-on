7d58f089ed4ffdf4c614ce1bb3fea1706fc5ac01
ALFAHADH <s.alfahadh.472@gmail.com>
2026-07-03 15:23:16 +0530

    adding pub key

--- ssh-key.tf
+++ ssh-key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "ssh_key" {
     key_name   = "aws-terraform-key"
-    public_key = file("C:/Users/ADMIN/aws-terraform-key.pub")
+    public_key = file("${path.module}/aws-terraform-key.pub")
 }
